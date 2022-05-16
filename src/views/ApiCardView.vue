48097fd74658f058b2e6afe1c4e6adfbeb3370c1
lysiandon <don.lysiane@gmail.com>
2022-05-16 15:00:51 +0200

    refactoring

--- src/views/ApiCardView.vue
+++ src/views/ApiCardView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper">
-    <ItemsAPI/>
+    <!-- <ItemsAPI/> -->
     <h3>SOURCE NUMBER {{id + 1}} : </h3>
     <h1> {{title}}</h1>
 
@@ -19,10 +19,10 @@
 </template>
 
 <script>
-import ItemsAPI from '../utils/ItemsApi.vue'
+// import ItemsAPI from '../utils/ItemsApi.vue'
 export default{
   name : 'apiCardView',
-  components : {ItemsAPI},
+  //   components : {ItemsAPI},
   data(){
     return{
       id : parseInt(this.$route.params.id),
@@ -36,7 +36,18 @@ export default{
   },
   async mounted(){
     //GET items' infos : 
-    ItemsAPI()
+    let items = await fetch('https://api.zotero.org/groups/4571976/items?token=g5mqcdecr82jl2a21t2j4xbo45xt4z7aur9leyoz')
+    items = await items.json()
+    items = items[this.id]
+    console.log(items)
+    this.title = items.data.title
+    this.author = items.meta.createdByUser.username
+    this.url = items.data.url
+    this.date = items.data.dateAdded
+    this.tags = items.data.tags
+    this.abstractNote = items.data.abstractNote
+    // ItemsAPI()
+
   }
 }
 </script>
